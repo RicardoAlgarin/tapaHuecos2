3a7e2dfe0f5e7e1aca920cef8fc9ffa25764ead1
ASUS <gitemail01@gmail.com>
2016-11-25 08:28:41 -0500

    Auth y registrer OK, backend funcionando, HOME NEWS FUNCIONANDO CON TOOL BAR , ICONO DE NUEVA NOTICIA Y MENU TOOLBAR. POSTACTIVITY EMPTY BUT OK.

--- app/src/main/java/com/sine/tapahuevos2/Home_News.java
+++ app/src/main/java/com/sine/tapahuevos2/Home_News.java
@@ -1,11 +1,13 @@
 package com.sine.tapahuevos2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Home_News extends AppCompatActivity {
@@ -15,15 +17,24 @@ public class Home_News extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__news);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+         setSupportActionBar(toolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_add){
+
+            startActivity(new Intent(Home_News.this, PostActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
 
 
