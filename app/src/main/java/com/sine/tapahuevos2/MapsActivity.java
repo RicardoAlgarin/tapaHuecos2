15fabcac2e919c231f77cc00e0efdeab2b9113c2
ASUS <gitemail01@gmail.com>
2016-11-30 00:57:37 -0500

    Auth y registrer OK, backend funcionando, HOME NEWS FUNCIONANDO CON TOOL BAR , ICONO DE NUEVA NOTICIA Y MENU TOOLBAR. POSTACTIVITY DESING OK, BACKEND :STORAGE IMAGE OK, TEXTOS TITLE Y DESC EN LA BASE DE DATOS + RETURN HOME_NEWS AFTER UPLOAD SUCCESSFULLY + CARDVIEW ON  OK HOME_NEWS, IMAGE:OK, PICASSO LIBRERY OK + USER ,IMAGE REGISTRATION DATABASE en el email_activity. + BACKPRESS JUMP ACTIVITY + ACTION_CLOSE OK + ACTION_SINGOUT OK +EMAIL PASSWORD FIXED +POST_TAKE_PHOTO ACTIVITY CAPTURE +HOME NEWS PUBLIC TAKEPHOTO:  ( TO PUBLIC TAKE_PHOTO TO HOME NEWS OK) + FAB OK :  (NEW ACTIVITY GOOGLE MAPS OK )+ LAUNCHER_ICON + IMAGEN ENTRADA OK+ POSICION ACTUAL EN EL FAB OK.

--- app/src/main/java/com/sine/tapahuevos2/MapsActivity.java
+++ app/src/main/java/com/sine/tapahuevos2/MapsActivity.java
@@ -1,18 +1,32 @@
 package com.sine.tapahuevos2;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker marcador;
+    double lat = 0.0;
+    double lng = 0.0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +39,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        miUbicacion();
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng monteria= new LatLng(8.76,-75.8);
-        mMap.addMarker(new MarkerOptions().position(monteria).title("Monteria"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(monteria));
+    private void agregarMarcador(double lat, double lng) {
+        LatLng coordenadas = new LatLng(lat, lng);
+        CameraUpdate miUbicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 16);
+        if (marcador != null) marcador.remove();
+        marcador = mMap.addMarker(new MarkerOptions()
+                .position(coordenadas)
+                .title("Hueco Actual")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_disc_full_black_24dp)));
+        mMap.animateCamera(miUbicacion);
+    }
+
+    private void actualizarUbicacion(Location location) {
+
+        if (location != null) {
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+            agregarMarcador(lat, lng);
+
+        }
+    }
+
+    LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            actualizarUbicacion(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void miUbicacion() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        actualizarUbicacion(location);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,15000,0,mLocationListener);
+
     }
 }
